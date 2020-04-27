7a8d83e4c05d60f57602f9f528541828ba7e7d53
KotdenGit <kotden09@gmail.com>
2020-04-28 01:23:27 +0300

    Update 01-strings-tasks.js

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /* *******************************************************************************************
  *                                                                                           *
  * Plese read the following tutorial before implementing tasks:                              *
